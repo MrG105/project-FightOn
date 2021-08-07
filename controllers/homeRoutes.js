3bc8deb5d2027d45e615bde4c2b9f594a8409be8
Gabriel Gutierrez <poo328@my.utsa.edu>
2021-08-07 10:16:35 -0500

    fixed routing for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const { User, Game } = require("../models/");
+const withAuth = require('../utils/auth');
+
 
 router.get('/', withAuth, async (req, res) => {
   try {
