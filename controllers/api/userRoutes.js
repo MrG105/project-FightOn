7095d1dfcd10665df61cd14edd95af836c941b6c
jacob sierra <jacobisaac22@yahoo.com>
2021-08-12 18:44:09 -0500

    updated links

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -118,6 +118,7 @@ router.post('/login', async (req, res) => {
             req.session.logged_in = true;
             res.json({ user: userData, message: 'You are now logged in!' });
         });
+        
     } catch (err) {
         res.status(400).json(err);
     }
